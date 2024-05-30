3c0d93ba7a86be9ed643f220c65f00254143ed35
Eric Sirianni <eric.sirianni@gmail.com>
2024-05-29 22:36:25 -0400

    Python boilerplate

---
+++ README.md
@@ -0,0 +1,1 @@
+# screenlogic-exporter
---
+++ main.py
@@ -0,0 +1,19 @@
+import asyncio
+import pprint
+
+from screenlogicpy import ScreenLogicGateway, discovery
+
+async def main():
+  hosts = await discovery.async_discover()
+
+  if len(hosts) > 0:
+      gateway = ScreenLogicGateway()
+      await gateway.async_connect(**hosts[0])
+      await gateway.async_update()
+      await gateway.async_disconnect()
+      pprint.pprint(gateway.get_data())
+  else:
+      print("No gateways found")
+
+
+asyncio.run(main())
